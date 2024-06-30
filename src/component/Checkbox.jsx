218641f509704634d42e7bb5ba3bc35e864f4239
143xx <71882579+m48483@users.noreply.github.com>
2024-06-30 20:03:57 +0900

    checkbox 수정

--- src/component/Checkbox.jsx
+++ src/component/Checkbox.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Checkbox = ({id, check = false}) => {
+const Checkbox = ({id, check = false, disabled = false}) => {
     return (
         <>
-            <input type="checkbox" id={id} defaultChecked={check} />
+            <input type="checkbox" id={id} defaultChecked={check} disabled={disabled}/>
             <label htmlFor={id}></label>
         </>
     )
 }
-
 export default Checkbox;
